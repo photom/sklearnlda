e3155359de4d3a503754de08553c9c8ae43c9e85
photom <photom@users.noreply.github.com>
2016-12-05 00:24:19 +0900

    modify online test docs data list

--- create_docs.py
+++ create_docs.py
@@ -10,27 +10,25 @@ CONTENT = "content"
 DOCS_DIR = "docs"
 NEWS_DIR = "news"
 
-ticker = "GOOGL"
-alnum = string.lowercase + string.digits
-if not os.path.exists(DOCS_DIR):
-    os.makedirs(DOCS_DIR)
+ticker_raws = ["GOOGL", "NVDA", "AMZN", "NFLX"]
+for ticker in ticker_raws:
+    alnum = string.lowercase + string.digits
+    if not os.path.exists(DOCS_DIR):
+        os.makedirs(DOCS_DIR)
 
-for picker_file in os.listdir(NEWS_DIR):
-    if not picker_file.find(ticker) == 0:
-        continue
-    news_path = os.path.join(NEWS_DIR, picker_file)
-    with open(news_path, mode='r') as f:
+    for picker_file in os.listdir(NEWS_DIR):
+        if not picker_file.find(ticker) == 0:
+            continue
+        news_path = os.path.join(NEWS_DIR, picker_file)
+        with open(news_path, mode='r') as f:
 
-        output_body = picker_file.replace(".pickle", "")
-        article_map = pickle.load(f)
-        texts = []
-        for idx, link in enumerate(article_map):
-            date_content = article_map[link]
-            text = date_content[CONTENT]
-            texts.append(text)
-            output_path = output_body + "_" + str(idx) + ".txt"
-            filename = "{dir}/{output}".format(dir=DOCS_DIR, output=output_path)
-            print (filename)
-            with open(filename, "wb") as f:
-                content = u"\r\n".join(texts)
-                f.write(content.encode('utf-8'))
+            output_body = picker_file.replace(".pickle", "")
+            article_map = pickle.load(f)
+            for idx, link in enumerate(article_map):
+                date_content = article_map[link]
+                text = date_content[CONTENT]
+                output_path = output_body + "_" + str(idx) + ".txt"
+                filename = "{dir}/{output}".format(dir=DOCS_DIR, output=output_path)
+                print (filename)
+                with open(filename, "wb") as f:
+                    f.write(text.encode('utf-8'))
--- stochastic_lda_sklearn.py
+++ stochastic_lda_sklearn.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import sys, re, time, string, random, csv, argparse
+import matplotlib.pyplot as plt
 import scipy
 import numpy as np
 from scipy.special import psi
@@ -10,10 +11,8 @@ from utils import *
 from sklearn.datasets import fetch_20newsgroups
 from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
 from sklearn.decomposition import NMF, LatentDirichletAllocation
-from scipy.sparse import csr_matrix
 # import matplotlib.pyplot as plt
 from nltk.corpus import wordnet
-# from nltk.stem.snowball import EnglishStemmer
 from nltk.stem import WordNetLemmatizer
 from functools import partial
 np.set_printoptions(threshold=np.inf)
@@ -44,61 +43,70 @@ def print_top_words(model, feature_names, n_top_words):
 
 def main():
     parser = argparse.ArgumentParser()
-    #parser.add_argument('-K', '--topics', help='number of topics, defaults to 10', required=True)
-    #parser.add_argument('-m', '--mode', help='mode, test | normal | stock', required=True)
-    #parser.add_argument('-v', '--vocab', help='Vocab file name, .csv', default="dictionary.csv", required=False)
     parser.add_argument('-d', '--docs', help='file with list of docs, .txt', default="alldocs.txt", required=False)
-    #parser.add_argument('-a', '--alpha', help='alpha parameter, defaults to 0.2', default=0.8, required=False)
-    #parser.add_argument('-e', '--eta', help='eta parameter, defaults to 0.2', default=0.2, required=False)
-    #parser.add_argument('-t', '--tau', help='tau parameter, defaults to 1024', default=1024, required=False)
-    #parser.add_argument('-k', '--kappa', help='kappa parameter, defaults to 0.7', default=0.7, required=False)
-    #parser.add_argument('-n', '--iterations', help='number of iterations, defaults to 10000', default=10000, required=False)
     parser.add_argument('-s', '--stopwords', help='stopwords, defaults to stopwords.txt', default='stopwords.txt',
                         required=False)
 
     args = parser.parse_args()
 
     K = 10
-    #mode = str(args.mode)
-    #K = int(args.topics)
-    #alpha = float(args.alpha)
-    #eta = float(args.eta)
-    #tau = float(args.tau)
-    #kappa = float(args.kappa)
-    #iterations = int(args.iterations)
     with open(args.stopwords) as f:
         stopwords = set(f.read().split())
     doc_files = str(args.docs)
     assert doc_files is not None, "no docs"
     docs = getalldocs(doc_files)
     D = len(docs)
-    n_features = 1000
+    n_features = 1024
     n_top_words = 20
 
     analyze_word_partial = partial(analyze_word, stopwords=stopwords)
-    tf_vectorizer = CountVectorizer(max_df=0.95, min_df=0.01, max_features=n_features,
+    tf_vectorizer = CountVectorizer(min_df=0.01, max_df=0.9, max_features=n_features,
                                     strip_accents='ascii',
                                     stop_words=stopwords, token_pattern=r"(?u)\b[^\W\d_][^\W\d_]+\b",
                                     preprocessor=None, analyzer=analyze_word_partial)
-
     tf = tf_vectorizer.fit_transform(docs)
     lda = LatentDirichletAllocation(n_topics=K, learning_method='online',
-                                    learning_decay=0.7,
+                                    learning_decay=0.9,
                                     learning_offset=1024,
-                                    max_iter=200,
+                                    max_iter=500,
                                     total_samples=D,
-                                    random_state=1234,
                                     )
-    lda.fit(tf)
+    transform_result = lda.fit_transform(tf)
+    trained_feature_names = tf_vectorizer.get_feature_names()
+    print_top_words(lda, trained_feature_names, n_top_words)
+    print("mean")
+    print(np.mean(transform_result, axis=0))
+    print("std")
+    print(np.std(transform_result, axis=0))
+    print(transform_result)
+    hist, bin_edges = np.histogram(transform_result[:, 0], bins=10)
+    print(bin_edges)
+    plt.bar(bin_edges[:-1], hist, width=1)
+    plt.show()
+    print(transform_result[:, 0])
+    testdoc = getfiles("docs/hogehoge.txt")
+    tf_vectorizer = CountVectorizer(min_df=1, max_df=1.0,
+                                    strip_accents='ascii',
+                                    stop_words=stopwords,
+                                    analyzer='word')
+    test_tf = tf_vectorizer.fit_transform([testdoc])
     tf_feature_names = tf_vectorizer.get_feature_names()
-    # print("names: " + str(tf_feature_names))
+    n_test_samples, n_test_features = test_tf.shape
+    print("testfeturenum:" + str(n_test_features) +
+          " tf_featre_names:" + str(len(tf_feature_names)))
+    if n_test_features < len(trained_feature_names):
+        for i in range(len(trained_feature_names) - n_test_features):
+            testdoc += " a" + str(i)
+    tf_vectorizer = CountVectorizer(min_df=1, max_df=1.0,
+                                    strip_accents='ascii',
+                                    stop_words=stopwords,
+                                    analyzer='word')
+    test_tf = tf_vectorizer.fit_transform([testdoc])
+    test_tf = lda.transform(test_tf)
+    print(test_tf)
+    tf_feature_names = tf_vectorizer.get_feature_names()
     print_top_words(lda, tf_feature_names, n_top_words)
 
-    testdocs = getalldocs("testdocs_googl.txt")
-    test_tf = tf_vectorizer.fit_transform(testdocs)
-    ret = lda.transform(test_tf)
-    print(len(ret))
-    sys.stdout.write(str(ret))
 
 if __name__ == '__main__':
     main()
